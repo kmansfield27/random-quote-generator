dd3f117e26465ed8b889a8a3e8e0ebceaae1167b
kmansfield27 <36116244+kmansfield27@users.noreply.github.com>
2020-07-10 23:00:45 -0400

    Remove console.log

--- js/script.js
+++ js/script.js
@@ -83,7 +83,6 @@ const printQuote = function() {
   const changeBackground = function() {
     const body = document.getElementsByTagName('body')[0];
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    console.log(randomColor);
     body.style.backgroundColor = `#${randomColor}`;
   }
 
