a884659427cf2e0c4c1a3cfd709bfc4a5a2627c7
kmansfield27 <36116244+kmansfield27@users.noreply.github.com>
2020-07-10 20:46:14 -0400

    Add initial files

---
+++ js/script.js
@@ -0,0 +1,65 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array
+***/
+
+var quotes = [
+  {
+    quote: 'It sounds plausible enough tonight, but wait until tomorrow. Wait for the common sense of the morning.',
+    source: 'H.G. Wells',
+    citation: 'The Time Machine',
+    year: 1895
+  },
+  {
+    quote: "It's much better to do good in a way that no one knows anything about it.",
+    source: 'Leo Tolstoy',
+    citation: 'Anna Karenina',
+    year: 1877
+  },
+  {
+    quote: "The best way to cheer yourself up is to try to cheer somebody else up.",
+    source: 'Mark Twain'
+  },
+  {
+    quote: "Better three hours too soon than a minute too late.",
+    source: 'William Shakespeare',
+    citation: 'The Merry Wives of Windsor'
+  },
+  {
+    quote: "Nothing is so painful to the human mind as a great and sudden change.",
+    source: 'Mary Shelley',
+    year: 1818
+  },
+
+
+
+]
+
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
